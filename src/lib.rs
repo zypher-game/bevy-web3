ef5a5822fcef2c529d2c026c87a10303968d53a9
Neo Sun <huachuang20@gmail.com>
2024-02-25 13:42:48 +1300

    Add send transaction

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use bevy::{
     prelude::*,
     tasks::{IoTaskPool, TaskPool},
 };
-use web3::transports::eip_1193;
+use web3::{transports::eip_1193, types::TransactionRequest};
 
-pub use web3::types::{H160, H520};
+pub use web3::types::{H160, H256, H520, U256};
 
 pub enum RecvError {
     Empty,
@@ -31,12 +31,12 @@ impl Plugin for WalletPlugin {
 
 #[derive(Resource)]
 pub struct WalletChannel {
-    account_tx: Sender<(H160, String)>,
-    account_rx: Receiver<(H160, String)>,
+    account_tx: Sender<(H160, U256)>,
+    account_rx: Receiver<(H160, U256)>,
     signature_tx: Sender<H520>,
     signature_rx: Receiver<H520>,
-    transaction_tx: Sender<H520>,
-    transaction_rx: Receiver<H520>,
+    transaction_tx: Sender<H256>,
+    transaction_rx: Receiver<H256>,
 }
 
 fn init_wallet(mut commands: Commands) {
@@ -64,10 +64,11 @@ impl WalletChannel {
                 let web3 = web3::Web3::new(transport);
 
                 let addrs = web3.eth().request_accounts().await.unwrap();
+                let chain = web3.eth().chain_id().await.unwrap();
 
                 if !addrs.is_empty() {
                     info!("addrs: {:?}", addrs);
-                    let _ = tx.send((addrs[0], "TODO_network".to_owned())).await;
+                    let _ = tx.send((addrs[0], chain)).await;
                 }
             })
             .detach();
@@ -82,26 +83,32 @@ impl WalletChannel {
                 let web3 = web3::Web3::new(transport);
 
                 let msg = web3::types::Bytes(msg.as_bytes().to_vec());
-                let signature = web3.personal().sign(msg, account, "").await.unwrap();
+                let signature = web3.eth().sign(account, msg).await.unwrap();
                 let _ = tx.send(signature).await;
             })
             .detach();
     }
 
-    pub fn send(&self) {
-        let _tx = self.transaction_tx.clone();
+    pub fn send(&self, from: H160, to: H160, data: Vec<u8>) {
+        let tx = self.transaction_tx.clone();
         IoTaskPool::get_or_init(TaskPool::new)
             .spawn(async move {
                 let provider = eip_1193::Provider::default().unwrap().unwrap();
                 let transport = eip_1193::Eip1193::new(provider);
-                let _web3 = web3::Web3::new(transport);
+                let web3 = web3::Web3::new(transport);
 
-                // TODO
+                let mut txr = TransactionRequest::default();
+                txr.from = from;
+                txr.to = Some(to);
+                txr.data = Some(data.into());
+
+                let hash = web3.eth().send_transaction(txr).await.unwrap();
+                let _ = tx.send(hash).await;
             })
             .detach();
     }
 
-    pub fn recv_account(&self) -> Result<(H160, String), RecvError> {
+    pub fn recv_account(&self) -> Result<(H160, U256), RecvError> {
         Ok(self.account_rx.try_recv()?)
     }
 
@@ -109,7 +116,7 @@ impl WalletChannel {
         Ok(self.signature_rx.try_recv()?)
     }
 
-    pub fn recv_transaction(&self) -> Result<H520, RecvError> {
+    pub fn recv_transaction(&self) -> Result<H256, RecvError> {
         Ok(self.transaction_rx.try_recv()?)
     }
 }
